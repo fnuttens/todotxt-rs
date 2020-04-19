72324f74ef299a1766d8af0e7a86456d8e49e538
Florent Nuttens <fnuttens@dabao.fr>
2020-04-19 17:41:24 +0200

    feat(lib): implement add command

---
+++ todotxt-lib/src/config.rs
@@ -0,0 +1,3 @@
+pub const TODOTXT_PATH: &str = "D:\\Documents\\Drive\\todo\\todo.txt";
+
+// TODO: manage a serialized configuration (RON or TOML)
--- todotxt-lib/src/lib.rs
+++ todotxt-lib/src/lib.rs
@@ -1,6 +1,11 @@
 mod config;
 
-use chrono::NaiveDate;
+use std::fs::OpenOptions;
+use std::io::Write;
+
+use chrono::{NaiveDate, Utc};
+
+use config::TODOTXT_PATH;
 
 pub fn add(
     todo: &str,
@@ -8,8 +13,57 @@ pub fn add(
     creation_date: Option<NaiveDate>,
     insert_creation_date: bool,
 ) -> Result<(), String> {
-    todo!();
+    let mut file = OpenOptions::new()
+        .append(true)
+        .open(TODOTXT_PATH)
+        .map_err(|e| e.to_string())?;
+
+    let new_todo = format_task(todo, priority, creation_date, insert_creation_date);
+    writeln!(file, "{}", new_todo).map_err(|e| e.to_string())?;
+    Ok(())
+}
+
+fn format_task(
+    todo: &str,
+    priority: Option<char>,
+    creation_date: Option<NaiveDate>,
+    insert_creation_date: bool,
+) -> String {
+    let mut task = String::new();
+
+    if let Some(priority_value) = priority {
+        let fmt_priority = format!("({}) ", priority_value);
+        task.push_str(fmt_priority.as_str());
+    }
+
+    if let Some(forced_creation_date) = creation_date {
+        let fmt_creation_date = format!("{} ", forced_creation_date);
+        task.push_str(fmt_creation_date.as_str());
+    } else if insert_creation_date {
+        let today = Utc::today();
+        let fmt_creation_date = format!("{} ", today.format("%Y-%m-%d"));
+        task.push_str(fmt_creation_date.as_str());
+    }
+
+    task.push_str(todo);
+    task
 }
 
 #[cfg(test)]
-mod tests {}
+mod should {
+    use super::*;
+
+    #[test]
+    fn format_tasks_correctly() {
+        assert_eq!("todo", format_task("todo", None, None, false));
+        assert_eq!(
+            "(A) 2020-05-02 todo",
+            format_task(
+                "todo",
+                Some('A'),
+                Some(NaiveDate::parse_from_str("2020-05-02", "%Y-%m-%d").unwrap()),
+                false
+            )
+        );
+    }
+}
